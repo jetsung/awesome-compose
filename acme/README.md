7782d15f0e8c247326fd43ae3a1a5861e1232b18
Jetsung Chan <jetsungchan@gmail.com>
2025-03-06 02:11:16 +0800

    pref(acme): 优化执行脚本

--- acme/README.md
+++ acme/README.md
@@ -1,9 +1,92 @@
 # acme.sh
 
-> acme.sh Docker 版   
-https://github.com/acmesh-official/acme.sh
+[Office Web][1] - [Source][2] - [Docker Image][3] - [Docment][4]
 
-## 手动添加
+---
+
+> [acme.sh][1] 是一个纯用 Shell（Unix shell）语言编写的 ACME 协议客户端。  
+- 完整实现了 ACME 协议。  
+- 支持 ECDSA 证书。  
+- 支持 SAN 和通配符证书。  
+- 简单、强大且非常易于使用。你只需要 3 分钟就能学会。  
+- 兼容 Bash、dash 和 sh。  
+- 完全用 Shell 编写，不依赖 Python。  
+- 只需一个脚本即可自动签发、续期和安装你的证书。  
+- 不需要 root 或 sudo 权限。  
+- 支持 Docker。  
+- 支持 IPv6。  
+- 支持通过 cron 任务发送续期或错误通知。
+
+[1]:https://github.com/acmesh-official/acme.sh
+[2]:https://github.com/acmesh-official/acme.sh
+[3]:https://hub.docker.com/r/neilpang/acme.sh
+[4]:https://github.com/acmesh-official/acme.sh/wiki
+
+## 使用教程
+
+1. 一键下载本源码
+```bash
+# 推荐
+# mkdir acme && cd $_
+curl -fsSL https://s.asfd.cn/a792ec34 | bash
+
+# 或
+git clone https://framagit.org/jetsung/docker-compose
+cp -r docker-compose/acme .
+# cd acme
+```
+
+2. [设置 API 密钥](https://github.com/acmesh-official/acme.sh/wiki/dnsapi)
+```bash
+# Cloudflare: dns_cf
+export CF_Key="<api_key>"
+export CF_Email="<email>"
+
+# DNSPod.cn: dns_dp
+export DP_Id="<id>"
+export DP_Key="<key>"
+
+# Aliyun: dns_ali
+export Ali_Key="<key>"
+export Ali_Secret="<secret>"
+
+# DNSPod.com: dns_dpi
+export DPI_Id="<id>"
+export DPI_Key="<key>"
+
+# TencentCloud: dns_tencent
+export Tencent_SecretId="<Your SecretId>"
+export Tencent_SecretKey="<Your SecretKey>"
+
+...
+```
+
+3. 先运行容器
+```bash
+docker compose up -d
+```
+
+4. 设置证书平台
+```bash
+# 切换 CA 为 Let's Encrypt，若曾有注册过，可跳过
+./deploy.sh -a ca -s letsencrypt
+
+# 或
+# 切换 CA 为 Let's Encrypt，并设置邮箱
+./deploy.sh -a ca -s letsencrypt -e me@example.com
+```
+
+5. 签发证书并且部署
+```bash
+# 申请证书
+./deploy.sh -a is -d example.com
+
+# 或通过 challenge 别名验证（推荐，方便统一校验。比如多个域名的 NS 在不同的平台）
+# 先将  _acme-challenge.example.com CNAME 记录到 _acme-challenge.challenge.com
+./deploy.sh -a is -d example.com -ch challenge.com
+```
+
+## 官方教程
 
 - https://github.com/acmesh-official/acme.sh/wiki/dns-manual-mode
 
@@ -24,4 +107,4 @@ docker exec acme.sh --renew --dns --keylength ec-256 --ecc --force --yes-I-know-
 docker exec acme.sh --install-cert --ecc --key-file /data/ssl/xx.com.key --fullchain-file /data/ssl/xx.com.fullchain.cer -d xx.com
 ```
 
-## [更多教程](https://forum.idev.top/d/525)
+## [更多文档说明](https://forum.idev.top/d/525)
--- acme/deploy.sh
+++ acme/deploy.sh
@@ -1,54 +1,197 @@
 #!/usr/bin/env bash
 
-# 部署 acme.sh
+#============================================================
+# File: deploy.sh
+# Description: 部署 ACME 证书
+# Author: Jetsung Chan <i@jetsung.com>
+# Version: 0.1.0
+# CreatedAt: 
+# UpdatedAt: 2025-03-05
+#============================================================
 
-set -euo pipefail
+if [[ -n "$DEBUG" ]]; then
+    set -eux
+else
+    set -euo pipefail
+fi
 
-BASE_EXEC=""
+# 输出错误信息并退出
+error_exit() {
+    echo -e "\033[31merror: $1\033[0m" >&2
+    exit 1
+}
 
-# CA 服务器
-CA_SERVER=""
+warn() {
+    echo -e "\033[33m$1\033[0m"
+}
 
-# CA Email
-CA_EMAIL=""
+tip() {
+    echo -e "\033[32m$1\033[0m"
+}
 
-# 运行定时任务
-CRON=""
-
-# 域名
-DOMAIN=""
-
-# DNS 类型
-DNS_TYPE="dns_cf"
-
-# 交换域名
-CHALLENGE=""
-
-# 扩展参数
-EXTEND=""
-
-# SSL 证书文件夹（按实际环境修改）
-SSL_PATH="/srv/acme/ssl"
-
-# 保存 SSL 证书的文件夹（Docker 中）
-SSL_SAVE_PATH="/data/ssl"
-
-# 最近一天内是否有文件修改
-MTIME=1
-
-# 更新日志
-UPDATE_LOG="./update.log"
-
-# 获取 base exec
-get_base_exec() {
+# 获取执行命令
+get_docker_exec() {
     if docker ps --filter "name=acme.sh" --quiet | grep -q .; then
-        # Docker 中
-        BASE_EXEC="docker exec acme.sh"
+        # Docker
+        DOCKER_EXEC="docker exec acme.sh acme.sh"
+        SSL_PATH="/data/ssl"
     else
-        # 服务器中
-        SSL_SAVE_PATH="$SSL_PATH"
+        # 服务器
+        DOCKER_EXEC="acme.sh"
+        if [[ -z "${SSL_PATH:-}" ]]; then
+            SSL_PATH="$SAVE_SSL_PATH"
+        fi
     fi
-    # echo "BASE_EXEC: $BASE_EXEC"
+}
+
+# 切换 CA 
+## https://letsencrypt.org 90 days
+## https://acme-v02.api.letsencrypt.org/directory
+## https://acme-staging-v02.api.letsencrypt.org/directory
+##
+## https://zerossl.com 90 days
+## https://acme.zerossl.com/v2/DV90
+##
+## https://www.buypass.com 180 days
+## https://api.buypass.com/acme/directory
+## https://api.test4.buypass.no/acme/directory
+##
+## https://cloud.google.com/certificate-manager/docs/public-ca-tutorial
+## https://dv.acme-v02.api.pki.goog/directory
+## https://dv.acme-v02.test-api.pki.goog/directory
+##
+## https://freessl.cn 90 days
+##
+do_setca() {
+    $DOCKER_EXEC --set-default-ca --server "$CA_SERVER"
+    
+    if [[ -n "${CA_EMAIL:-}" ]]; then
+        if [[ -n "${EAB_KID:-}" ]] && [[ -n "${EAB_HMAC_KEY:-}" ]]; then
+            $DOCKER_EXEC --register-account --server "$CA_SERVER" --email "$CA_EMAIL" \
+                --eab-kid "$EAB_KID" --eab-hmac-key "$EAB_HMAC_KEY"
+        else
+            $DOCKER_EXEC --register-account --server "$CA_SERVER" --email "$CA_EMAIL"
+        fi
+    fi    
+}
+
+# 显示帮助信息 CA
+help_setca() {
+    cat <<EOF
+用法: $0 -a $ACTION [options]
+      $(warn "设置 CA")
+选项:
+    -h,  --help                              显示 CA 帮助信息
+    -s,  --server <server>                   设置 CA 服务器, 默认 letsencrypt
+    -e,  --email <email>                     可选, 设置 CA 邮箱
+
+    -ek, --eab-kid <eab-kid>                 可选, 设置 EAB 密钥 ID
+    -eh, --eab-hmac-key <eab-hmac-key>       可选, 设置 EAB HMAC 密钥
+
+EOF
+}
+
+# 签发证书
+do_issue() {
+    if [[ -z "${DOMAIN:-}" ]]; then
+        error_exit "错误: 域名 (domain) 不能为空."
+    fi
+    if [[ -z "${DNS_TYPE:-}" ]]; then
+        error_exit "错误: DNS 类型 (dns) 不能为空."
+    fi
+
+    # shellcheck disable=SC2206
+    ARGS=(
+        --ecc
+        --force
+        -d "$DOMAIN"
+        -d "*.$DOMAIN"
+        --dns "$DNS_TYPE"
+        --keylength ec-256
+        ${EXTEND:-}
+    )
+
+    [ -n "${CHALLENGE:-}" ] && ARGS+=(--challenge-alias "$CHALLENGE")
+
+    # 签发并部署证书
+    $DOCKER_EXEC --issue "${ARGS[@]}" && \
+    do_deploy
+}
+
+help_issue() {
+    cat <<EOF
+用法: $0 -a $ACTION [options]
+      $(warn "签发证书")
+选项:
+    -h,  --help                              显示帮助信息
+    -d,  --domain <domain>                   记录域名
+    -ns, --dns <cf,ali,dp,dpi>               NS 类型, 默认 dns_cf
+            dns_cf                           Cloudflare
+            dns_ali                          阿里云
+            dns_dp                           DNSPod 中国版
+            dns_dpi                          DNSPod 国际版
+            ...                              更多请查看 acme.sh 文档
+    -ch, --challenge <challenge>             可选, 交换域名
+    -ex, --extend <extend>                   可选, 扩展参数
+
+EOF
+}
+
+# 部署证书
+do_deploy() {
+    if [[ -z "${DOMAIN:-}" ]]; then
+        error_exit "错误: 域名 (domain) 不能为空."
+    fi
+
+    $DOCKER_EXEC --install-cert \
+        --ecc \
+        -d "$DOMAIN" \
+        --key-file "${SSL_PATH}/${DOMAIN}.key" \
+        --fullchain-file "${SSL_PATH}/${DOMAIN}.fullchain.cer"
+}
+
+# 定时任务
+do_cron() {
+    case "${SETUP:-}" in
+    "1")
+        # 安装定时任务
+        script_path=$(dirname "$(readlink -f "$0")")
+        script_name=$(basename "$0")
+
+        # 随机生成分钟（0-59）
+        MINUTE=$((RANDOM % 60))
+
+        # 随机生成小时（0-7）
+        HOUR=$((RANDOM % 8))
+
+        echo "$MINUTE $HOUR * * * root cd $script_path; /bin/bash ./$script_name --action"
+        # 输出随机生成的 Cron 时间
+        cat > /etc/cron.d/dockeracme <<EOF
+$MINUTE $HOUR * * * root cd $script_path; /bin/bash ./$script_name --action cron
+EOF
+        systemctl restart cron
+        ;;
+        
+    *)
+        # 运行定时任务
+        if find "$SAVE_SSL_PATH" -type f -mtime -"$MTIME" | grep -q .; then
+            restart_server | tee -a "$UPDATE_LOG"
+        fi            
+        ;;        
+    esac    
+}
+
+# 显示帮助信息 定时任务
+help_cron() {
+    cat <<EOF
+用法: $0 -a $ACTION [options]
+      $(warn "执行定时任务")
+选项:
+    -h,  --help                              显示帮助信息
+    -se, --setup                             可选, 安装计划任务（无参数则运行计划任务）
+    -mt, --mtime <mtime>                     可选, 证书更新时间, 默认 1
+    
+EOF
 }
 
 # 服务器重载命令（按实际环境修改）
@@ -74,153 +217,106 @@ restart_server() {
     fi
 }
 
-# 切换 CA
-set_ca() {
-    local server="${CA_SERVER:-letsencrypt}"
-    local email="${CA_EMAIL:-}"
-
-    # 切换 CA 为 Let's Encrypt
-    if [ -z "$email" ]; then
-        $BASE_EXEC acme.sh --set-default-ca --server "$server"
+# 备份本服务
+do_backup() {
+    dir_name="${PWD##*/}"
+    bk_file_name="${dir_name}-$(date +%Y%m%d).tar.xz"
+    if tar -caf "../${bk_file_name}" -C "${PWD%/*}" "$dir_name"; then
+        mv "../${bk_file_name}" .
+        echo "备份成功: $bk_file_name"
     else
-        $BASE_EXEC acme.sh --register-account --server "$server" -m "$email"
+        echo "备份失败"
     fi
 }
 
-# 设置或运行计划任务
-set_cron() {
-    case "${CRON:-}" in
-        # 安装定时任务
-        "install")
-            script_path=$(dirname "$(readlink -f "$0")")
-            script_name=$(basename "$0")
+# 显示帮助信息 备份
+help_backup() {
+    cat <<EOF
+用法: $0 -a $ACTION [options]
+      $(warn "备份本服务")
+选项:
+    -h,  --help                              显示帮助信息
 
-            # 随机生成分钟（0-59）
-            MINUTE=$((RANDOM % 60))
-
-            # 随机生成小时（0-23）
-            HOUR=$((RANDOM % 24))
-
-            # 输出随机生成的 Cron 时间
-            cat > /etc/cron.d/dockeracme <<EOF
-$MINUTE $HOUR * * * root cd $script_path; /bin/bash ./$script_name -c
 EOF
-            systemctl restart cron
-            ;;
-        
-        # 运行定时任务
-        "run")
-            if find "$SSL_PATH" -type f -mtime -"${MTIME:-1}" | grep -q .; then
-                restart_server | tee -a "$UPDATE_LOG"
-            fi            
-            ;;
-
-        *)
-            echo "error: Please specify the correct cron type."
-            exit 1
-            ;;
-    esac
-}
-
-# 签发和部署域名
-deploy_domain() {
-    # 签发
-    if [ -z "$CHALLENGE" ]; then
-        $BASE_EXEC acme.sh --issue \
-            --ecc \
-            --force \
-            -d "$DOMAIN" \
-            -d "*.$DOMAIN" \
-            --dns "$DNS_TYPE" \
-            --keylength ec-256 \
-            "$EXTEND"
-    else
-        $BASE_EXEC acme.sh --issue \
-            --ecc \
-            --force \
-            -d "$DOMAIN" \
-            -d "*.$DOMAIN" \
-            --dns "$DNS_TYPE" \
-            --challenge-alias "$CHALLENGE" \
-            --keylength ec-256 \
-            "$EXTEND"    
-    fi
-
-    # 部署
-    $BASE_EXEC acme.sh --install-cert \
-        --ecc \
-        -d "$DOMAIN" \
-        --key-file "$SSL_SAVE_PATH/$DOMAIN.key" \
-        --fullchain-file "$SSL_SAVE_PATH/$DOMAIN.fullchain.cer"
 }
 
 # 处理参数信息
 judgment_parameters() {
     while [[ "$#" -gt '0' ]]; do
-        case "$1" in
-            '-a' | '--challenge')
-                # 交换域名
+        case "${1,,}" in
+            '-h' | '--help')
+                # 显示帮助
+                HELP=1
+                ;;
+
+            '-a' | '--action') 
+                # 操作
                 shift
-                CHALLENGE="${1:?"error: Please specify the correct challenge."}"
-                ;;
-
-            '-c' | '--cron')
-                # 定时任务
-                CRON="run"
-                if [ $# -ge 2 ] && [[ "$2" != -* ]]; then
-                    CRON="install"
-                    shift         
-                fi
-                ;;
-
-            '-e' | '--email')
-                # CA Email
-                shift
-                CA_EMAIL="${1:?"error: Please specify the correct email."}"
-                ;;
-
-            
+                ACTION="${1:?"错误: 操作类型 (action) 不能为空."}"
+                ACTION="${ACTION,,}" # 转小写
+                ;;    
             '-s' | '--server')
                 # CA 服务器
                 shift
-                CA_SERVER="${1:?"error: Please specify the correct server."}"
+                CA_SERVER="${1:?"错误: CA 服务器 (server) 不能为空."}"
                 ;;
-            
-            '-p' | '--path')
-                # SSL 证书文件夹
+            '-e' | '--email')
+                # CA 邮箱
                 shift
-                SSL_PATH="${1:?"error: Please specify the correct path."}"
-                ;; 
+                CA_EMAIL="${1:?"错误: CA 邮箱 (email) 不能为空."}"
+                ;;  
 
             '-d' | '--domain')
-                # 记录域名
+                # 签发域名
                 shift
-                DOMAIN="${1:?"error: Please specify the correct domain."}"
+                DOMAIN="${1:?"错误: 域名 (domain) 不能为空."}"
                 if [[ "$DOMAIN" != *"."* ]]; then
-                    echo -e "\033[31mDOMAIN must be a domain name\033[0m"
-                    exit 1
+                    error_exit "错误: 域名 (domain) 必须包含点."
+                fi                
+                ;;
+
+            '-ek' | --eab-kid)
+                # EAB 密钥 ID
+                shift
+                EAB_KID="${1:?"错误: EAB 密钥 ID (eab-kid) 不能为空."}"
+                ;;
+            '-eh' | --eab-hmac-key)
+                # EAB HMAC 密钥
+                shift
+                EAB_HMAC_KEY="${1:?"错误: EAB HMAC 密钥 (eab-hmac-key) 不能为空."}"
+                ;;
+
+            '-ns' | '--dns')
+                # DNS 类型
+                shift
+                DNS_TYPE="${1:?"错误: DNS 类型 (dns) 不能为空."}"
+                DNS_TYPE="${DNS_TYPE,,}"
+                if [[ "$DNS_TYPE" != "dns_"* ]]; then
+                    DNS_TYPE="dns_$DNS_TYPE"
                 fi
-                ;;
-            
-            '-x' | '--extend')
-                # 扩展证书
+                ;;                
+            '-ch' | '--challenge')
+                # 交换域名
                 shift
-                EXTEND="${1:?"error: Please specify the correct extend."}"
-                ;;
-
-            # DNS 类型
-            '-n' | '--dns')
+                CHALLENGE="${1:?"错误: 交换域名 (challenge) 不能为空."}"
+                ;;                
+            '-ex' | '--extend')
+                # 扩展参数
                 shift
-                DNS_TYPE="${1:?"error: Please specify the correct dns."}"
-                ;;
+                EXTEND="${1:?"错误: 扩展参数 (extend) 不能为空."}"
+                ;;     
 
-            # 显示帮助
-            '-h' | '--help')
-                show_help
-                ;;
+            '-se' | '--setup')
+                SETUP=1
+                ;;    
+            '-mt' | '--mtime')
+                # 最近几天内是否有文件修改
+                shift
+                MTIME="${1:?"错误: 最近几天内是否有文件修改 (mtime) 不能为空."}"
+                ;;       
 
             *)
-                echo "$0: unknown option -- $1" >&2
+                echo "$0: 未知选项 -- $1" >&2
                 exit 1
                 ;;
         esac
@@ -229,51 +325,94 @@ judgment_parameters() {
 }
 
 # 显示帮助信息
-# 显示帮助信息
-show_help() {
+show_top_help() {
     cat <<EOF
-Usage: $0 [options]
+用法: $0 [options]
+    $(warn "部署 ACME 证书")
+选项:
+    -h,  --help                              显示帮助信息
+    -a,  --action     <ca|is>                执行操作
+            ca setca                         切换 CA
+            is issue                         签发证书
+            cr cron                          计划任务
+            bk backup                        执行备份
 
-Options:
-  -h, --help               Show this help message
-  -a, --challenge <domain> Set challenge domain
-  -c, --cron [install|run] Set or run cron job
-  -e, --email <email>      Set CA email
-  -s, --server <server>    Set CA server
-  -p, --path <path>        Set SSL certificate path
-  -d, --domain <domain>    Set domain name
-  -x, --extend <args>      Add extend arguments
-  -n, --dns <dns>          Set DNS provider
-
-Example:
-  $0 -c install -e user@example.com -s zerossl
-  $0 -a challenge.example.com -p /srv/ssl -d example.com -n dns_cf
 EOF
     exit 0
 }
 
-# 执行
-do_action() {
-    if [ -n "$CA_SERVER" ]; then
-        set_ca
-    fi
+# 显示帮助信息
+show_help() {
+    case "${ACTION:-}" in
+        'ca' | 'setca')
+            help_setca
+            ;;
 
-    if [ -n "${CRON:-}" ]; then
-        set_cron
-    fi
+        'is' | 'issue')
+            help_issue
+            ;;
 
-    if [ -n "$DOMAIN" ] && [ -n "$DNS_TYPE" ]; then
-        deploy_domain
-    fi
+        'cr' | 'cron')
+            help_cron
+            ;;
+
+        'bk' | 'backup')
+            help_backup
+            ;;
+
+        *)
+            show_top_help
+            ;;
+    esac
+    exit 0
 }
 
-# 主函数
 main() {
-    get_base_exec
+    judgment_parameters "$@"    
 
-    judgment_parameters "$@"
+    if [[ $# -eq 0 ]]; then
+        HELP=1
+    fi
+    
+    if [[ -n "${HELP:-}" ]]; then
+        show_help
+    fi
 
-    do_action
+    SAVE_SSL_PATH="$(pwd)/data/ssl"
+
+    get_docker_exec
+
+    MTIME="${MTIME:-1}" # 最近一天内是否有文件修改
+    UPDATE_LOG="update.log" # 更新日志
+
+    DNS_TYPE="${DNS_TYPE:-dns_cf}" # DNS 类型
+    CA_SERVER="${CA_SERVER:-letsencrypt}" # CA 服务器
+
+    case "${ACTION:-}" in
+        'ca' | 'setca')
+            # 设置 CA
+            do_setca
+            ;;
+
+        'is' | 'issue')
+            # 签发
+            do_issue
+            ;;
+
+        'cr' | 'cron')
+            # 定时任务
+            do_cron
+            ;;
+
+        'bk' | 'backup')
+            # 备份
+            do_backup
+            ;;
+
+        *)
+            error_exit "无效的操作类型."
+            ;;
+    esac
 }
 
 main "$@"
